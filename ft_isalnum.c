b1e462fd8b144cdc89d302599ace77c33d45956b
Judy Nkwama <nkwamajudy@gmail.com>
2023-05-08 21:07:02 +0200

    Instaling the tester

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -12,12 +12,10 @@
 
 #include "libft.h"
 
-int ft_isalnum(int c)
+int	ft_isalnum(int c)
 {
-	if(ft_isalpha(c) == 1 || ft_isdigit(int c) == 1)
+	if (ft_isalpha(c) == 1 || ft_isdigit(int c) == 1)
 		return (1);
 	else
 		return (0);
-
-
 }
--- libft.h
+++ libft.h
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junkwama <junkwama@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 20:31:42 by junkwama          #+#    #+#             */
+/*   Updated: 2023/05/08 20:37:48 by junkwama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
+
 # define LIBFT_H
 # include <stdlib.h>
 # include <stdbool.h>
 
-int ft_isalpha(int c);
-int ft_isdigit(int c); 
-int ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
 
 #endif
